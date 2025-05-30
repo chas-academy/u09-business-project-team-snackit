1c68371bfd2c3c923f079ee6e78658d10116e3d4
Chokladglasyr <lim_ida@live.com>
2025-05-30 11:09:47 +0200

    feat: added getUser

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -9,5 +9,58 @@ export const getUsers = async (req: Request, res: Response) => {
             return;
         }
         res.json(users);
+    } catch (err: unknown) {
+        if(err instanceof Error) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+    }
+}
+
+export const getUser = async (req: Request, res: Response) => {
+    try {
+        //change this to get userId from the logged in information
+        const user = await User.findById(req.params.userId);
+        if(!user) {
+            res.status(404).json({message: "User not found."});
+            return;
+        }
+        res.json(user);
+
+    } catch (err: unknown) {
+        if(err instanceof Error) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+    }
+}
+export const createUser = async (req: Request, res: Response) => {
+    try {
+
+    } catch (err: unknown) {
+        if(err instanceof Error) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+    }
+}
+export const updateUser = async (req: Request, res: Response) => {
+    try {
+
+    } catch (err: unknown) {
+        if(err instanceof Error) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+    }
+}
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+
+    } catch (err: unknown) {
+        if(err instanceof Error) {
+            res.status(500).json({error: err.message});
+            return;
+        }
     }
 }
