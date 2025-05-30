bd9d3ca56e555bf03f962b5bea0c50d276e0d81f
Chokladglasyr <lim_ida@live.com>
2025-05-30 10:58:51 +0200

    feat: added getUsers

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,2 +1,13 @@
-import {request, response} from 'express';
+import { Request, Response} from 'express';
+import { User } from '../models/userModel';
 
+export const getUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await User.find().select("-password -confirmed_password");
+        if(!users || users.length === 0) {
+            res.status(404).json({message: "No users found."})
+            return;
+        }
+        res.json(users);
+    }
+}
