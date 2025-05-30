63b49c6c2eb7c80cccc909ffe929e6a592433bd7
Lazcano007 <Pedrolazcano@live.com>
2025-05-30 15:28:18 +0200

    added oauth dependencies

---
+++ backend/server.ts
@@ -0,0 +1,17 @@
+import express, {Express, Request, Response} from "express";
+import passport from "passport";
+import { Strategy as GoogleStrategy} from "passport-google-oauth20";
+import session from "express-session";
+import cors from "cors";
+import { connect } from "mongoose";
+import connectDB from 
+
+
+
+connect()
+
+
+const clientID = "";
+const clientSecret ="";
+
+passport.use()
