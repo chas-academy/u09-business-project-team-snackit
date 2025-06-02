1ffb6d3aa6ddee0ccf3b3402b64614a77542cf9d
Chokladglasyr <lim_ida@live.com>
2025-06-02 15:16:38 +0200

    styles: added styles and components for lobby buttons

---
+++ frontend/src/Leaderboard.tsx
@@ -0,0 +1,9 @@
+function Leaderboard() {
+
+    return(
+        <>
+        <p>you're not number 1</p>
+        </>
+    )
+}
+export default Leaderboard;
--- frontend/src/Lobby.tsx
+++ frontend/src/Lobby.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Lobby() {
 
     return(
         <>
        <h1>Welcome username!</h1>
+       <nav>
+       <Link to={"/game"} id="start-game" className="lobby-btn"><img src="start-btn.svg" alt=""  className="lobby-img"/>START</Link>
+       <Link to={"/leaderboard"} id="leaderboard" className="lobby-btn"><img src="iconoir_leaderboard-star.svg" alt="" />LEADERBOARD</Link>
+       <Link to={"/profile"} id="profile" className="lobby-btn"><img src="pajamas_profile.svg" alt="" />PROFILE</Link>
+       </nav>
         </>
     )
 }
