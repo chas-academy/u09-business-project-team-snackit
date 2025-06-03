46686a1bce621c839cc5589fdbbaa23b6a8ef43e
Chokladglasyr <lim_ida@live.com>
2025-06-03 11:35:12 +0200

    feat: added update function, and env api variable

--- frontend/src/Profile.tsx
+++ frontend/src/Profile.tsx
@@ -1,16 +1,90 @@
+import { Link } from "react-router-dom";
 import BackBtn from "./components/back-btn";
+import { useEffect, useState } from "react";
 
 function Profile() {
-    const deleteUser = async () => {
+    const API_URL = import.meta.env.NODE_ENV === "prod" ? import.meta.env.VITE_API_BASE_URL_PROD : import.meta.env.VITE_API_BASE_URL_LOCAL
+        const [user, setUser] = useState("");
+      
+        const fetchUser = async () => {
+          const res = await fetch (`${API_URL}/auth/user`, {
+            credentials: "include"
+          });
+          
+          const data = await res.json();
+          const userData = await fetch(`${API_URL}/users/${data.id}`);
+          const user = await userData.json();
+          setUser(user);
+        };
+        
+        useEffect(() => {
+          fetchUser();
+        }, []);
 
+        useEffect(() => {
+            if(user){
+                setFormData({
+                    name: user.name || "",
+                    email: user.email || ""
+                })
+            }
+        }, [user])
+
+        const deleteUser = async () => {
+            try{
+
+            }catch(err: unknown) {
+                if(err instanceof Error) {
+                    console.error("Failed to delete: ", err);
+                    return;
+                }
+            }
+        }
+        const [formData, setFormData] = useState({
+            name: "",
+            email: "",
+        })
+        const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+            const {name, value } = e.target;
+            setFormData({...formData, [name]: value})
+        }
+    const updateUser = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try{
+            const res = await fetch(`${API_URL}/users/${user._id}`, {
+                credentials: "include",
+                method: "PUT",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({...formData})
+            })
+            const data = await res.json();
+            console.log(data)
+            window.location.reload();
+        }catch(err: unknown) {
+            if(err instanceof Error) {
+                console.error("Update user failed: ", err)
+            }
+        }
     }
   return (
     <>
       <header>
+        <Link to={''} className="back-btn" onClick={deleteUser}>DELETE</Link>
         <BackBtn />
-        <Link to={''} onClick={deleteUser}>DELETE</Link>
       </header>
-      <main></main>
+      <main>
+        <section>
+        <h1>Welcome {user.name}!</h1>
+        <img src="img_1.svg" alt="fox in a chefshat" /> 
+        </section>
+        <article>
+            <form className="update-form" onSubmit={updateUser}>
+                <input type="text" name="name" placeholder="Name: "  value={formData.name} onChange={handleInput}/>
+                <input type="text" name="email" placeholder="Email: " value={formData.email} onChange={handleInput}/>
+                <button>UPDATE</button>
+            </form>
+        </article>
+      </main>
     </>
   );
 }
