e2d57da6e9d6f3f0be2fc3dfd6f47b5cc26a2ade
Lazcano007 <Pedrolazcano@live.com>
2025-06-04 01:12:59 +0200

    Added: instruction-page (design and code) and added websocket for the players to be able to face eachother

--- frontend/src/Lobby.tsx
+++ frontend/src/Lobby.tsx
@@ -7,7 +7,7 @@ function Lobby() {
         <>
        <h1>Welcome username!</h1>
        <nav>
-       <Link to={"/game"} id="start-game" className="lobby-btn"><img src="start-btn.svg" alt=""  className="lobby-img"/>START</Link>
+       <Link to={"/instructions"} id="start-game" className="lobby-btn"><img src="start-btn.svg" alt=""  className="lobby-img"/>START</Link>
        <Link to={"/leaderboard"} id="leaderboard" className="lobby-btn"><img src="iconoir_leaderboard-star.svg" alt="" />LEADERBOARD</Link>
        <Link to={"/profile"} id="profile" className="lobby-btn"><img src="pajamas_profile.svg" alt="" />PROFILE</Link>
        </nav>
