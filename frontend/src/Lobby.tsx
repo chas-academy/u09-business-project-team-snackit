22763d4f0d79cd63ca2b1ea91cbf1b89fcfff436
Lazcano007 <Pedrolazcano@live.com>
2025-06-04 20:34:39 +0200

    Added: feched correct username in lobby when loggin in

--- frontend/src/Lobby.tsx
+++ frontend/src/Lobby.tsx
@@ -1,11 +1,18 @@
+
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useFetchUser } from "./hooks/useFetchUser";
 
 function Lobby() {
 
+    const { user, loading, error } = useFetchUser();
+    if(loading) return <p>Loading...</p>
+    if (error) return <p>Error: {error.message}</p>;
+    if(!user ) return <p>User not found</p>
+
     return(
         <>
-       <h1>Welcome username!</h1>
+       <h1>Welcome {user.name}!</h1>
        <nav>
        <Link to={"/instructions"} id="start-game" className="lobby-btn"><img src="start-btn.svg" alt=""  className="lobby-img"/>START</Link>
        <Link to={"/leaderboard"} id="leaderboard" className="lobby-btn"><img src="iconoir_leaderboard-star.svg" alt="" />LEADERBOARD</Link>
