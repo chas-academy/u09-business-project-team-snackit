a1437f55fd418172d3b5622cb47dd77611976c95
Lazcano007 <Pedrolazcano@live.com>
2025-06-04 21:10:09 +0200

    Added: more properties that we need in the gamemodel

--- backend/models/gameModel.ts
+++ backend/models/gameModel.ts
@@ -1,12 +1,22 @@
 import mongoose, {Document, Schema} from "mongoose";
 
 export interface IGame extends Document {
+    players: string[];
+    currentTurn: string;
+    currentIngredient: string;
+    lives: {[playerId: string]: number};
+    status: "playing" | "finished" ;
     winner: string;
     loser: string;
 }
 
 
 const gameSchema: Schema = new Schema ({
+    players: {type: [String], required: true},
+    currentTurn: { type: String, required: true },
+    currentIngredient: { type: String, required: true},
+    lives: {type: Map, of: Number, required: true},  // visar antalet liv spelare har kvar 
+    status: {type: String, enum: ["playing", "finished"], default: "playing"},  // visar spelets tilstånd 
     winner: { type: String, requred: true},
     loser: {type: String, required: true},
 })
