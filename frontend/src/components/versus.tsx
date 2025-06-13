a385d087e8b88fed1d3840fee37310bc94b4fe98
Chokladglasyr <lim_ida@live.com>
2025-06-14 00:13:16 +0200

    feat: fetch a hardcoded guest player

--- frontend/src/components/versus.tsx
+++ frontend/src/components/versus.tsx
@@ -1,42 +1,46 @@
+import { useState } from "react";
 import { useFetchUser } from "../hooks/useFetchUser";
+import { useFetchGuest } from "../hooks/useFetchGuest";
 
 function Versus() {
-    const { user, loading, error} = useFetchUser();
+  const { user, loadingUser, errorUser } = useFetchUser();
+const {guest, loadingGuest, errorGuest} = useFetchGuest();
 
+  if (loadingUser || loadingGuest) return <p> Loading...</p>;
+  if (errorUser) return <p>Error: {errorUser.message}</p>;
+  if (errorGuest) return <p>Error: {errorGuest.message}</p>;
+  if (!user) return <p>User not found</p>;
+  if (!guest) return <p>Player Two not found</p>
+  // console.log(user)
 
-    if (loading) return <p> Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
-    if (!user) return <p>User not found</p>;
-    // console.log(user)
-    const guest = {
-        name: "Guest",
-        profilePic: "img_1.svg",
-        lives: 3, 
-    };
+  return (
+    <>
+      <section className="versus">
+        <div className="player">
+          <p className="player-name">{user.name}</p>
+          <img
+            className="smaller-pic"
+            src={user.profilePic}
+            alt="player profile pic"
+          />
+          <div className="hearts">{"❤️".repeat(3)}</div>
+        </div>
 
-    return (
-        <>
-        
-        <section className="versus"> 
-                <div className="player">
-                    <p className="player-name">{user.name}</p>
-                    <img className="smaller-pic" src={user.profilePic} alt="player profile pic" />
-                    <div className="hearts">{"❤️".repeat(3)}</div>
-                </div>
-               
-        
-                <div className="vs-icon">
-                    {/* <p>VS</p>    lägg in "vs" bilden som ida gjort HÄR */}
-                        <img id="vsImg" src="vs.svg" alt="red pepper vs garlic" />
-                </div>
+        <div className="vs-icon">
+          <img id="vsImg" src="vs.svg" alt="red pepper vs garlic" />
+        </div>
 
-                <div className="player">
-                    <p className="player-name">{guest.name}</p>
-                    <img className="smaller-pic" src={guest.profilePic} alt="opponent profile pic" />
-                    <div className="hearts">{"❤️".repeat(guest.lives)}</div>
-                </div>
-            </section>
-        </>
-    )
+        <div className="player">
+          <p className="player-name">{guest.name}</p>
+          <img
+            className="smaller-pic"
+            src={guest.profilePic}
+            alt="opponent profile pic"
+          />
+          <div className="hearts">{"❤️".repeat(3)}</div>
+        </div>
+      </section>
+    </>
+  );
 }
 export default Versus;
---
+++ frontend/src/hooks/useFetchGuest.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+const API_URL =
+  import.meta.env.NODE_ENV === "prod"
+    ? import.meta.env.VITE_API_BASE_URL_PROD
+    : import.meta.env.VITE_API_BASE_URL_LOCAL;
+
+type Guest = {
+  _id: string;
+  name: string;
+  email: string;
+  wins: number;
+  losses: number;
+  profilePic: string;
+  lives: string;
+};
+export const useFetchGuest = () => {
+  const [guest, setGuest] = useState<Guest | null>(null);
+    const [loadingGuest, setLoadingGuest] = useState(true);
+    const [errorGuest, setErrorGuest] = useState<Error | null>(null);
+
+  const fetchGuest = async () => {
+    try {
+      const res = await fetch(`${API_URL}/users/683d9a05a9cbce37c219c55b`, {
+        credentials: "include",
+      });
+      if (!res.ok) throw new Error("Failed to fetch guest");
+      const data = await res.json();
+      console.log(data);
+      setGuest(data);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.error(err);
+        setErrorGuest(err);
+      }
+    } finally {
+        setLoadingGuest(false);
+    }
+  };
+  useEffect(() => {
+    fetchGuest();
+  },[])
+  return {guest, loadingGuest, errorGuest}
+};
