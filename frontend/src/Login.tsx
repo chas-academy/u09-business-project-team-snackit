6eb00d549c7ccfa92af4af9b61f7b93f5fcdc31b
Chokladglasyr <lim_ida@live.com>
2025-06-16 01:17:06 +0200

    fix: using .MODE instead of .NODE_ENV

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,6 +1,6 @@
 function Login() {
   const API_URL =
-    import.meta.env.NODE_ENV === "prod"
+    import.meta.env.MODE === "production"
       ? import.meta.env.VITE_API_BASE_URL_PROD
       : import.meta.env.VITE_API_BASE_URL_LOCAL;
 
