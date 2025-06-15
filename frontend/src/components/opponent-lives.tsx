61b60b522d0e923463349571650e79c9ce0b0cb5
Chokladglasyr <lim_ida@live.com>
2025-06-15 11:49:59 +0200

    fix: in opponents its the games players lives that are showing

--- frontend/src/components/opponent-lives.tsx
+++ frontend/src/components/opponent-lives.tsx
@@ -1,8 +1,27 @@
+import { useFetchGame } from "../hooks/useFetchGame";
+import { useFetchGuest } from "../hooks/useFetchGuest";
+
 function OpponentLives({gameId}: {gameId: string}) {
+        const {ongoingGame, loadingOngoingGame, errorOngoingGame} = useFetchGame(gameId!);
+    
+        const playerTwo = ongoingGame?.players[1]
+        // console.log(playerTwo)
+        const {guest, loadingGuest, errorGuest} = useFetchGuest(playerTwo!);
+
+        if (errorGuest) return <p>Error: {errorGuest.message}</p>;
+        if (!guest) return <p>Player Two not found</p>
+        if (loadingGuest) return <p> Loading guest...</p>;
+    
+        if (loadingOngoingGame) return <p>Loading ongoing game...</p>
+        if (errorOngoingGame) return <p>Error: {errorOngoingGame.message}</p>
+        if (!ongoingGame) return <p>Ongoing game not found</p>
 
     return (
         <>
-                    <div className="hearts">{"❤️".repeat(3)}</div>
+          {ongoingGame.lives && guest._id in ongoingGame.lives && (
+            
+            <div className="hearts">{"❤️".repeat(ongoingGame.lives[guest._id])}</div>
+          )}
         </>
     )
 }
