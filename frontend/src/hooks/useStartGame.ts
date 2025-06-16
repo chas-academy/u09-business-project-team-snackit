69bdf4e366a862692b00793b973aef517815e1eb
Chokladglasyr <lim_ida@live.com>
2025-06-16 12:13:57 +0200

    refactor: deleted console.logs and formatted

--- frontend/src/hooks/useStartGame.ts
+++ frontend/src/hooks/useStartGame.ts
@@ -6,42 +6,42 @@ const API_URL =
     : import.meta.env.VITE_API_BASE_URL_LOCAL;
 
 type Game = {
-    _id: string,
-    players: string[],
-    currentTurn: string,
-    currentIngredient: string,
-    lives: Record<string, number>,
-    score: number,
-    status: string,
-    winner: string,
-    loser: string
-}
+  _id: string;
+  players: string[];
+  currentTurn: string;
+  currentIngredient: string;
+  lives: Record<string, number>;
+  score: number;
+  status: string;
+  winner: string;
+  loser: string;
+};
 export const useFetchGameStart = (gameId: string) => {
-    const [game, setGame] = useState<Game | null>(null);
-    const [loadingGame, setLoadingGame] = useState(true);
-    const [errorGame, setErrorGame] = useState<Error | null>(null)
+  const [game, setGame] = useState<Game | null>(null);
+  const [loadingGame, setLoadingGame] = useState(true);
+  const [errorGame, setErrorGame] = useState<Error | null>(null);
 
-    const fetchGameStart = useCallback(async() => {
-        try {
-            const res = await fetch(`${API_URL}/api/games/start/${gameId}`, {
-                credentials: "include",
-                method: "POST"
-            })
-            if(!res.ok) throw new Error("Failed to start game");
-            const data = await res.json();
-            console.log(data)
-            setGame(data);
-        } catch(err: unknown) {
-            if(err instanceof Error) {
-                console.error(err)
-                setErrorGame(err)
-            }
-        } finally {
-            setLoadingGame(false);
-        }
-    }, [gameId])
-    useEffect(() => {
-        fetchGameStart()
-    }, [fetchGameStart])
-    return {game, loadingGame, errorGame}
-}
+  const fetchGameStart = useCallback(async () => {
+    try {
+      const res = await fetch(`${API_URL}/api/games/start/${gameId}`, {
+        credentials: "include",
+        method: "POST",
+      });
+      if (!res.ok) throw new Error("Failed to start game");
+      const data = await res.json();
+
+      setGame(data);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.error(err);
+        setErrorGame(err);
+      }
+    } finally {
+      setLoadingGame(false);
+    }
+  }, [gameId]);
+  useEffect(() => {
+    fetchGameStart();
+  }, [fetchGameStart]);
+  return { game, loadingGame, errorGame };
+};
