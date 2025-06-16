73c12bda0cc7f62a21246ac522db8ce56be28b77
Chokladglasyr <lim_ida@live.com>
2025-06-16 11:28:46 +0200

    refactor: missed a to save gaminG?

--- frontend/src/components/gaming.tsx
+++ frontend/src/components/gaming.tsx
@@ -4,7 +4,7 @@ import { useFetchGameStart } from "../hooks/useStartGame";
 
 function Gaming() {
     const API_URL =
-  import.meta.env.NODE_ENV === "prod"
+  import.meta.env.MODE === "production"
     ? import.meta.env.VITE_API_BASE_URL_PROD
     : import.meta.env.VITE_API_BASE_URL_LOCAL;
 
