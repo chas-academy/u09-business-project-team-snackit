bc0897a15cbcdd37de0e0fed270db2046be75b94
Lazcano007 <Pedrolazcano@live.com>
2025-06-16 14:05:02 +0200

    Added: Change the name of a route so its more consistent with the rest of the routes

--- backend/routes/gameRoutes.ts
+++ backend/routes/gameRoutes.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/leaderboard", getLeaderboard);
 router.post("/", createGame);
 router.get("/:gameId", getGameById)
-router.post("/Start/:gameId",startGame);
+router.post("/start/:gameId",startGame);
 router.post("/:gameId/submit", checkSubmission);
 router.post("/:gameId/next", nextIngredient);
 router.post("/:gameId/forfeit", forfeitGame);
